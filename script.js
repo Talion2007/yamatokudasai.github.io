9f39d3eb3ef365025e40a19767ab72603edc9056
TalionElessar2007 <cagnin.lima.2007@gmail.com>
2024-10-03 11:06:55 -0300

    V1.hehe

---
+++ script.js
@@ -0,0 +1,30 @@
+document.querySelector("h1").innerText = "Kyo Ne to Chiisana Manco";
+
+document.getElementById("Buttom").addEventListener("click", function () {
+  document.getElementById("Message").innerText = "Open the console (;-;)";
+});
+
+//? Está isso funcionando ? Acho que sim
+let Mizu = "Kon'nichiwa​";
+let Desu = "Vitor Hugo";
+let OchaUno = Mizu + ", " + Desu + "!";
+console.log(OchaUno);
+
+//* Yamato Kudasaiiiii O~O
+// let sominha = 5 + 10
+// let resultado = "Berimbadas: " + sominha;
+// console.log(resultado);
+
+//! Top demais esses comentários coloridos
+let Gohan = "Sekkusu";
+let OchaDuo = `${Gohan}, ${Desu}? Kudasai? (O~O)`;
+console.log(OchaDuo);
+
+// Todo mundo vai pirar com essas cores
+let OchaTri = `Yamato Kudasaiiiiiiiiii... Yamato Kudasaiiiii, ${Desu}... Yamato... Kuda... SAIIIIIIIIII (>O<)`;
+console.log(OchaTri);
+
+//? Está gostando da história?
+let Sushi = "Arigatou Gozai Masu";
+let OchaQuo = `Awawwww, ${Desu}! ${Sushi}! (^O^)`;
+console.log(OchaQuo);
